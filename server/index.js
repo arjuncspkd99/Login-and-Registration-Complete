19e24bc835ddd509e3bb795d27fbb8a10da0a943
abc <arjuncspkd99@gmail.com>
2023-02-25 18:32:35 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -12,58 +14,51 @@ app.use(bodyParser.json());
 app.use(cors());
 require('dotenv').config();
 mongoose.set('strictQuery', true);
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-const User = mongoose.model('User', { email: String, password: String });
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
 
-      app.post('/api/register', (req, res) => {
-        const { email, password } = req.body;
-      
-        User.findOne({ email }, (err, user) => {
-          if (err) {
-            console.error('Error finding user in database:', err);
-            return res.status(500).json({ error: 'Server error' });
-          }
-          if (user) {
-            // If the user already exists, return an error response with a message
-            return res.status(409).json({ error: 'User already exists' });
-          }
-      
-          const newUser = new User({ email, password });
-          newUser.save((err) => {
-            if (err) {
-              console.error('Error inserting data into MongoDB:', err);
-              return res.status(500).json({ error: 'Server error' });
-            }
-      
-            console.log('Data was inserted into MongoDB:', newUser);
-            res.json({ message: 'Data was inserted into MongoDB' });
-          });
-        });
-      });
+const User = mongoose.model('User', userSchema);
 
-app.post('/api/login', (req, res) => {
+app.post('/api/register', async (req, res) => {
   const { email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const user = new User({ email, password: hashedPassword });
+    await user.save();
+    console.log('Data was inserted into MongoDB:', user);
+    res.json({ message: 'Data was inserted into MongoDB' });
+  } catch (err) {
+    console.error('Error inserting data into MongoDB:', err);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
 
-  User.findOne({ email }, (err, user) => {
-    if (err) {
-      console.error('Error finding user in database:', err);
-      return res.status(500).json({ error: 'Server error' });
-    }
+app.post('/api/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
     if (!user) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
-    if (password !== user.password) {
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
-
-    res.json({ message: 'Logged in successfully' });
-    
-  });
+    console.log('Logged in successfully');
+    return res.json({ message: 'Logged in successfully' });
+  } catch (err) {
+    console.error('Error finding user in database:', err);
+    res.status(500).json({ error: 'Server error' });
+  }
 });
 
 app.get('*', (req, res) => {
